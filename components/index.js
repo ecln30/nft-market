d7299ce0d714cff1f439191c8505b4bb07ea4897
ecln30 <ecln30@gmail.com>
2022-08-16 02:06:28 +0900

    first commit

---
+++ components/index.js
@@ -0,0 +1,52 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import HomeHeader from './HomeHeader'
+import FocusedStatusBar from './FocusedStatusBar'
+import NFTCard from './NFTCard'
+
+export {
+    HomeHeader,NFTCard,FocusedStatusBar
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ screens/Home.js
@@ -0,0 +1,93 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+import {useState} from 'react'
+import {View,SafeAreaView,FlatList,Text} from 'react-native'
+import {COLORS,NFTData} from '../constants'
+import {NFTCard,HomeHeader,FocusedStatusBar} from '../components'
+
+function Home() {
+  return (
+   <SafeAreaView style={{flex:1}}> 
+     
+   </SafeAreaView>
+  )
+}
+
+export default Home
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
